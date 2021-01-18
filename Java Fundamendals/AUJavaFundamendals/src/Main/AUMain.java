53bdd8c480b11d2c2f18114db4c2e7b659a54cd4
kalaichelvan-kn <kalaichelvan294@gmail.com>
2021-01-18 18:10:20 +0530

    upto java fundamentals

--- Java Fundamendals/AUJavaFundamendals/src/Main/AUMain.java
+++ Java Fundamendals/AUJavaFundamendals/src/Main/AUMain.java
@@ -1,11 +1,24 @@
 package Main;
 
+import Session1.Assignment1;
+
 public class AUMain {
+    public static void Session1(){
+        Assignment1 ass1 = new Assignment1();
+        ass1.readXml();
+        ass1.deSerialize();
+        ass1.serialize();
+        ass1.printTop5Ranks();
+        ass1.writeToCSV();
+        ass1.singletonExample();
+    }
+
+    public static void Session2(){
+
+    }
+
     public static void main(String args[]){
-        AUString auString=new AUString();
-        System.out.println(("hello World!!!"));
-        auString.string();
-        auString.stringBuffer();
-        auString.stringBuilder();
+        Session1();
+        Session2();
     }
 }
